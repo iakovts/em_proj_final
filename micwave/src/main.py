da5364414a0ec3005ac9e3919ed626c319921598
iakovos <itsouros@auth.gr>
2021-09-23 06:00:21 +0300

    Clean-up microwave class

--- micwave/src/main.py
+++ micwave/src/main.py
@@ -1,5 +1,6 @@
 import fdtd
 import numpy as np
+import os
 
 from micwave.util.config import cfg
 from micwave.util.helpers import gpt
@@ -9,15 +10,9 @@ from micwave.util.masks import mask_item, obj_on_grid, obj_indices
 class MicrowaveOven:
     def __init__(self, freq):
         self.foodstuff = ["plate", "burger", "potato1", "potato2"]
-        perm = cond = np.ones(
-            (gpt(cfg.dims.oven.x), gpt(cfg.dims.oven.y), gpt(cfg.dims.oven.z))
-        )
-        print(perm.shape)
         self.grid = fdtd.Grid(
             (cfg.dims.oven.x, cfg.dims.oven.y, cfg.dims.oven.z),
             grid_spacing=cfg.grid.spacing,
-            # permittivity=perm,
-            # permeability=cond,
         )
         self.min_height = 0  # Counter for z-axis current occupied height.
         self.b_thickness = 5  # Boundary thickness
@@ -25,10 +20,13 @@ class MicrowaveOven:
         self.f_var = None  # Frequency dependent variables of objs
         self.obj_pos = {}  # Contains the grid points of objects
         self.obj_indices = {}  # Object indices, used for post-processing
+        self.source_power = 117  # Source power in (V/m)
         if self.freq == 915:
             self.f_var = cfg.f915
+            self.freq *= 10 ** 6
         else:
             self.f_var = cfg.f2450
+            self.freq *= 10 ** 6
 
     def add_boundaries(self):
         # fmt: off
@@ -41,23 +39,16 @@ class MicrowaveOven:
         self.min_height += self.b_thickness
         # fmt: on
 
-    def obj_slices(self, dims):
-        """Returns a tuple of slices, used for a creating a rectangle around an
-        object to be placed in the oven"""
-        obj_x, obj_y = (
-            slice(gpt(dims.center[i]) - gpt(dims.r), gpt(dims.center[i]) + gpt(dims.r))
-            for i in range(2)
+    def add_source(self):
+        # lower left corner of source on y-z plane
+        source_corner = (gpt(0.06), gpt(0.06))
+        # source_x = slice(self.b_thickness, gpt(cfg.dims.source.x))
+        source_y = slice(source_corner[0], source_corner[0] + gpt(cfg.dims.source.y))
+        source_z = slice(source_corner[1], source_corner[1] + gpt(cfg.dims.source.z))
+        print(type(source_y), type(source_z), type(self.b_thickness + 1))
+        self.grid[self.b_thickness: self.b_thickness + 1, source_y, source_z] = fdtd.PlaneSource(
+            period=(1 / self.freq), power=self.source_power
         )
-        if dims.z is not None:
-            # Cylindrical
-            obj_z = slice(self.min_height, self.min_height + gpt(dims.z))
-        else:
-            # Spherical
-            obj_z = slice(self.min_height, self.min_height + 2 * gpt(dims.r))
-        return (obj_x, obj_y, obj_z)
-
-    def slc_len(self, slc):
-        return int(slc.stop - slc.start)
 
     def add_objects(self):
         for obj in self.foodstuff:
@@ -92,13 +83,31 @@ class MicrowaveOven:
             if obj == "plate":
                 self.min_height += gpt(dims.z)
 
+    def obj_slices(self, dims):
+        """Returns a tuple of slices, used for a creating a rectangle around an
+        object to be placed in the oven"""
+        obj_x, obj_y = (
+            slice(gpt(dims.center[i]) - gpt(dims.r), gpt(dims.center[i]) + gpt(dims.r))
+            for i in range(2)
+        )
+        if dims.z is not None:
+            # Cylindrical
+            obj_z = slice(self.min_height, self.min_height + gpt(dims.z))
+        else:
+            # Spherical
+            obj_z = slice(self.min_height, self.min_height + 2 * gpt(dims.r))
+        return (obj_x, obj_y, obj_z)
+
+    def slc_len(self, slc):
+        return int(slc.stop - slc.start)
+
 
 if __name__ == "__main__":
     oven = MicrowaveOven(915)
     oven.add_boundaries()
+    oven.add_source()
     oven.add_objects()
-   # oven.grid.visualize(
-   #     z=10, save=True, folder=""
-   # )
-   # # oven.grid.visualize(z=0)
-    # oven.grid[1:100, 2:30, 4:10] = fdtd.AbsorbingObject()
+    oven.grid.visualize(z=10, save=True, folder=os.getcwd())
+    oven.grid.run(total_time=500)
+# # oven.grid.visualize(z=0)
+# oven.grid[1:100, 2:30, 4:10] = fdtd.AbsorbingObject()
